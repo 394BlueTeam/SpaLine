0d6ee704dab367292a16ad041848196745d0fd0e
davidmichaelryan <davidryan2015@u.northwestern.edu>
2014-05-15 19:41:53 -0500

    copy of spaline.github.io

--- js/geosearch.js
+++ js/geosearch.js
@@ -1,89 +1,89 @@
 var map;
 var service;
 var infowindow;
-var initialLocation;
 var browserSupportFlag =  new Boolean();
+var requestLocation;
 
-
-function initialize() {
-
-  /*=== Initialize the map ===*/
-  initialLocation = new google.maps.LatLng(42.053317, -87.672788);
-
-  var myOptions = {
-    zoom: 6,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: initialLocation
+function getUserLocation(){
+  if(navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition(requestSalonInformation, handleLocationErrors);
   }
-  var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-
-  /*=== Get the user's location ===*/
-
-  // // Try W3C Geolocation (Preferred)
-  // if(navigator.geolocation) {
-  //   browserSupportFlag = true;
-  //   navigator.geolocation.getCurrentPosition(function(position) {
-  //     initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-  //     map.setCenter(initialLocation);
-  //   }, function() {
-  //     handleNoGeolocation(browserSupportFlag);
-  //   });
-  // }
-  // // Browser doesn't support Geolocation
-  // else {
-  //   browserSupportFlag = false;
-  //   handleNoGeolocation(browserSupportFlag);
-  // }
-
-
-/*=== Create a places request ===*/
-  
-  var request = {
-    location: initialLocation,
-    types: ['hair_care'],
-    // radius: '10000',
-    // rankBy: google.maps.places.RankBy.PROMINENCE
-    rankBy: google.maps.places.RankBy.DISTANCE
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+  else
+  {
+    console.log("Handle no geolocation 1");
+      browserSupportFlag = false;
+      handleNoGeolocation(browserSupportFlag);
+  }
 }
 
-function callback(results, status) {
+function requestSalonInformation(position){
+  if(position)
+  {
+  requestLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var myOptions = {
+              zoom: 6,
+              mapTypeId: google.maps.MapTypeId.ROADMAP,
+              center: requestLocation
+            }
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+  map.setCenter(requestLocation);
+  console.log("Requesting Location is: "+ requestLocation);
+  var request = {
+                  location : requestLocation,
+                  types: ['hair_care'],
+                  rankBy: google.maps.places.RankBy.DISTANCE
+                };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, getResultDetails);
+  }
+  else
+  {
+    console.log("Handle no geolocation");
+    handleNoGeolocation(true);
+  }
+}
+
+function getResultDetails(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
         var request = {
             reference: results[i].reference
         }
-        service.getDetails(request, callbackTwo);
+        service.getDetails(request, getResultDistance);
     }
   }
 }
 var distanceService = new google.maps.DistanceMatrixService();
 
-function callbackTwo(place, status) {
+function getResultDistance(place, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
         distanceService.getDistanceMatrix(
       {
-        origins: [initialLocation],
+        origins: [requestLocation],
         destinations: [place.geometry.location],
         travelMode: google.maps.TravelMode.DRIVING,
         unitSystem: google.maps.UnitSystem.IMPERIAL
-      }, function(response, status){addSalon(place, response.rows[0].elements[0].duration.text)});
+      }, function(response, status){addSalon(place, response.rows[0].elements[0].distance.text)});
   }
 }
 
 function addSalon(place, distance){
-    console.log(place.types)
-    var html = "<li class='col-sm-4 col-md-3 thumbnail'>"
-    html += "<img src='"+place.icon+"'></img>"
-    if (place.website != undefined){
-        html += "<div class='caption'><h3 class='name'><a href='salon.html?ref="+place.reference+"'>"+place.name+"</a></h3>"
-    }   
-    else{
-        html += "<div class='caption'><h3 class='name'>"+place.name+"</h3>"
+    console.log(place);
+    pic = insertPic(place.photos);
+    if (pic != '') {
+      var html = "<li class='col-sm-4 col-md-3 thumbnail'>";
+      html += pic; 
     }
+
+    else {
+      var html = "<li class='col-sm-4 col-md-3 thumbnail'>";
+    }
+
+    
+    html += "<div class='caption'><h3 class='name'><a href='salon.html?ref="+place.reference+"'>"+place.name+"</a></h3>";
     html += "<p class='address'>"+place.formatted_address+"</p>"
     html += "<p class='distance'>"+distance+"</p>"
     html += "</div></div></li>"
@@ -93,30 +93,86 @@ function addSalon(place, distance){
     $('#salonList').append(html);
 }
 
-/* === map generation (later) === */
 
-// function createMarker(place) {
-//   var placeLoc = place.geometry.location;
-//   var marker = new google.maps.Marker({
-//     map: map,
-//     position: place.geometry.location
-//   });
+function insertPic(data) {
+    if (!data) {
+      var pic = "";
+      return pic;
+    }
 
-//   google.maps.event.addListener(marker, 'click', function() {
-//     infowindow.setContent(place.name);
-//     infowindow.open(map, this);
-//   });
-// }
+    else {
+      var url = data[0].getUrl({'maxWidth': 150, 'maxHeight': 150});
+      // console.log(url);
+      var pic = "<img src='"+url+"'>";
+      // console.log(pic);
+      return pic;
+      // console.log(pic);
+    } 
+}
 
-  // function handleNoGeolocation(errorFlag) {
-  //   if (errorFlag == true) {
-  //     alert("Geolocation service failed.");
-  //     initialLocation = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-  //   } else {
-  //     alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-  //     initialLocation = new google.maps.LatLng(60, 105);
-  //   }
-  //   map.setCenter(initialLocation);
-  // }
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    console.log("Geolocation services failed");
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+    console.log("Your browser does not support geolocation services");
+  }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+function handleLocationErrors(error){
+  console.log("Error in retriving location");
+  switch(error.code)
+  {
+    case error.PERMISSION_DENIED:
+      handlePermissionDenied();
+      break;
+    case error.POSITION_UNAVAILABLE:
+      handlePositionUnavailable();
+      break;
+    case error.TIMEOUT:
+      handleTimeout();
+      break;
+    case error.UNKOWN_ERROR:
+      handleUnknown();
+      break;
+  }
+}
+
+function handlePermissionDenied(){
+  console.log("ERROR: User denied permission to location services");
+  var x = $('.wrapper');
+  x.append("<h3>Sorry, you need to allow location services to see this page</h3>");
+}
+
+function handlePositionUnavailable(){
+  console.log("ERROR: User's Position is unavailable");
+  var x = $('.wrapper');
+  x.append("<h3>Sorry, it looks like your position is temporarily unavailable</h3>");
+  x.append("<p>Check your network connection and make sure you have location services enabled, then try again</p>");
+}
+
+function handleTimeout(){
+  console.log("ERROR: Timeout occurred when getting location");
+  var x = $('.wrapper');
+  x.append("<h3>Sorry, it looks like a timeout occurred when trying to get your location</h3>");
+  x.append("<p>Check your network connection and try again</p>");
+}
+
+function handleUnknown(){
+  console.log("ERROR: An unknown error occurred");
+  var x = $('.wrapper');
+  x.append("<h3>Sorry, it looks like an unknown error occurred</h3>");
+  x.append("<p>Please try your request again</p>");
+}
+
+google.maps.event.addDomListener(window, 'load', getUserLocation);
